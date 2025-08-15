787f5831d31caa2e2ff1a2285ea017be55b4f170
Harsh Thakur <harhthakur9415@gmail.com>
2025-08-15 10:03:40 +0530

    initial setup to start

--- app/routes/aws_routes.py
+++ app/routes/aws_routes.py
@@ -3,6 +3,9 @@ from app.services.aws_service import start_instance, stop_instance
 
 router = APIRouter(prefix="/aws", tags=["AWS Control"])
 
+#/aws/start/gpu or /aws/start/cpu or /aws/start/instance id
+#similar for stoping 
+#will need to update it to orchastrator which will automatically on/off instance based on need
 @router.post("/start/{instance_id}")
 def start_ec2(instance_id: str):
     return start_instance(instance_id)
